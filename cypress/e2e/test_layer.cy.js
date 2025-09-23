c6ee46303df4cf357ee6f6b51626d3d394bed972
Thierry Gerbeau <thierry.gerbeau@ign.fr>
2025-09-23 11:37:27 +0200

    add layer first test

---
+++ cypress/e2e/test_layer.cy.js
@@ -0,0 +1,19 @@
+describe('Module Couche de données', () => {
+  it('affiche et active une couche de données', () => {
+    cy.visit('https://ignf.github.io/cartes.gouv.fr-editeur-carto/')
+    cy.contains('Se connecter').click()
+    cy.get('[name="username"]').type(Cypress.env('username'))
+    cy.get('[name="password"]').type(Cypress.env('password'))
+    cy.get('[data-action-id="login"]').click();
+    cy.contains('Mon espace').should('be.visible')
+    cy.get('.create').click() 
+    cy.get('[id^="GPshowLayersListPicto-"]').click()
+    //cy.get('[id^="GPlayersList-"]    // Ouvre le module Couche de données (adapte le sélecteur si besoin)
+    cy.contains('Couches de données').click()
+
+    cy.get('[id^="GPlayerSwitcher_ID_"]').should('have.length.greaterThan', 0)
+
+    // Vérifie l'effet sur la carte (adapte le sélecteur ou la vérification selon ton application)
+    cy.get('.ol-viewport').should('be.visible')
+      })
+})
