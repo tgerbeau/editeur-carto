31bfd4582f71f69552a2936b5de6d89705f6a520
Thierry Gerbeau <thierry.gerbeau@ign.fr>
2025-10-07 17:19:25 +0200

    add env variable (test)

--- cypress/e2e/test_hw.cy.js
+++ cypress/e2e/test_hw.cy.js
@@ -1,5 +1,6 @@
 describe('Hello World Test', () => {
   it('affiche hello world dans les logs', () => {
+     cy.get('[name="username"]').type(Cypress.env('username'))
     cy.log('hello world')
   })
 })
