d8974168b7b90622e9be1adbc68fbcbcd4b6b873
Thierry Gerbeau <thierry.gerbeau@ign.fr>
2025-10-08 10:43:54 +0200

    try displaying variables

--- cypress/e2e/test_hw.cy.js
+++ cypress/e2e/test_hw.cy.js
@@ -1,6 +1,7 @@
 describe('Hello World Test', () => {
   it('affiche hello world dans les logs', () => {
-     cy.get('[name="username"]').type(Cypress.env('username'))
-    cy.log('hello world')
+     //cy.get('[name="username"]').type(Cypress.env('username'))
+     cy.log('Username: ' + Cypress.env('username'))
+     cy.log('Password: ' + Cypress.env('password'))
   })
 })
