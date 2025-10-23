ed913021e253f48bcbabe0b3fbef9d8b9f8072f7
Thierry Gerbeau <thierry.gerbeau@ign.fr>
2025-10-23 15:38:42 +0200

    try fix CI

--- cypress/e2e/test_hw.cy.js
+++ cypress/e2e/test_hw.cy.js
@@ -2,8 +2,8 @@ describe('Hello World Test', () => {
   it('affiche hello world dans les logs', () => {
      cy.visit(Cypress.env('baseUrl'))
      cy.contains('Se connecter').click()
-     const username = Cypress.env('CYPRESS_USERNAME');
-     const password = Cypress.env('CYPRESS_PASSWORD');
+     const username = Cypress.env('CYPRESS_USERNAME') || 'default_value';
+     const password = Cypress.env('CYPRESS_PASSWORD')|| 'default_value';
      cy.log('Username: ' + username)
      cy.log('Password: ' + password)
      cy.get('[name="username"]').type(username)
