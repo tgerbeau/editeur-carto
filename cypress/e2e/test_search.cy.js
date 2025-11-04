03ec0f2a447b6257da7ead189b89d2d2aacaae70
Thierry Gerbeau <thierry.gerbeau@ign.fr>
2025-11-04 11:41:00 +0100

    fix search

--- cypress/e2e/test_search.cy.js
+++ cypress/e2e/test_search.cy.js
@@ -7,10 +7,10 @@ describe('Recherche d\'adresse', () => {
     cy.get('[data-action-id="login"]').click();
     cy.contains('Mon espace').should('be.visible')
     cy.get('.create').click() 
-    cy.get('input[placeholder*="Rechercher un lieu"]').type('10 Rue de la Paix, 50100 Cherbourg-en-Cotentin{enter}')
-    cy.get('input[placeholder*="Rechercher un lieu"]').should('have.length.greaterThan', 0)
+    cy.get('[id^="GPsearchInputText-32"]').type('10 Rue de la Paix, 50100 Cherbourg-en-Cotentin{enter}')
+    cy.get('[id^="GPsearchInputText-32"]').should('have.length.greaterThan', 0)
     // Sélectionne le premier élément de la liste déroulante des résultats
-    cy.get('input[placeholder*="Rechercher un lieu"]').first().click()
+    cy.get('[id^="GPsearchInputText-32"]').first().click()
     // Vérifie que le résultat sélectionné s'affiche sur la carte ou dans le champ (adapte le sélecteur si besoin)
     cy.contains('10 Rue de la Paix, 50100 Cherbourg-en-Cotentin').should('be.visible')
   })
